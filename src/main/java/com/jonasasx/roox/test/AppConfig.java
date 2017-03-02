22ea6d88d552b4f4e8040c348e9842ea6a1ec02d
jonasasx <jonasasx@gmail.com>
2017-03-02 04:24:11 +0300

    add param interceptor

--- src/main/java/com/jonasasx/roox/test/AppConfig.java
+++ src/main/java/com/jonasasx/roox/test/AppConfig.java
@@ -1,13 +1,52 @@
 package com.jonasasx.roox.test;
 
+import com.jonasasx.roox.test.entities.Customer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * Created by ionas on 01.03.17.
  */
 
 @Configuration
-//@EnableWebMvc
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new HandlerInterceptor() {
+			@Override
+			public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+				final Map<String, String> pathVariables = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+				String cid = pathVariables.get("cid");
+				if ("@me".equals(cid)) {
+					Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+					if (customer != null) {
+						pathVariables.put("cid", customer.getId().toString());
+						request.setAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE, pathVariables);
+					}
+				}
+				return true;
+			}
+
+			@Override
+			public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+			}
+
+			@Override
+			public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+			}
+		});
+		super.addInterceptors(registry);
+	}
 }
--- src/main/java/com/jonasasx/roox/test/controllers/ApiController.java
+++ src/main/java/com/jonasasx/roox/test/controllers/ApiController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@PreAuthorize("#cid == null || #cid == principal.id")
+@PreAuthorize("#cid == null || #cid == principal.id || #cid == '@me'")
 public class ApiController {
 
 	@Autowired
